566ff30d6b1f6cee3df5080e0ff2bcdaec58bb7d
golmschenk <greg@olmschenk.com>
2024-06-23 22:11:38 -0400

    Switch to not using the capitalized version of figure

--- scripts/light_curve_visualization.py
+++ scripts/light_curve_visualization.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 from bokeh.io import show
-from bokeh.plotting import figure as Figure
+from bokeh.plotting import figure
 
 from qusi.experimental.application.tess import TessMissionLightCurve
 
@@ -10,7 +10,7 @@ def main():
     light_curve_path = Path(
         'data/spoc_transit_experiment/train/positives/<add_downloaded_file_name_here>.fits')
     light_curve = TessMissionLightCurve.from_path(light_curve_path)
-    light_curve_figure = Figure(x_axis_label='Time (BTJD)', y_axis_label='Flux')
+    light_curve_figure = figure(x_axis_label='Time (BTJD)', y_axis_label='Flux')
     light_curve_figure.scatter(x=light_curve.times, y=light_curve.fluxes)
     light_curve_figure.line(x=light_curve.times, y=light_curve.fluxes, line_alpha=0.3)
     show(light_curve_figure)
